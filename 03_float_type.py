e4f46df376ac41c3dfafc356e271c85a629d47d9
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 15:15:06 +0100

    inspect the limits

--- 03_float_type.py
+++ 03_float_type.py
@@ -17,3 +17,38 @@ print(5e-324)
 print(1e-325)
 
 print(type(10.1))
+
+a = 3.14
+b = -2.718
+c = 0.0
+print(a, b, c)
+print(type(a))
+
+
+x = 10
+y = float(x)
+print(y)
+print(type(y))
+
+z = 7.89
+print(int(z))
+
+value = 3.1415926535
+print(round(value, 2))
+print(round(value, 4))
+print(round(value))
+
+x = 0.1 + 0.2
+print(x)
+print(x == 0.3)
+
+import math
+print(math.isclose(0.1 + 0.2, 0.3))
+
+import sys
+print(sys.float_info)
+print("Maximum:", sys.float_info.max)
+print("Minimum:", sys.float_info.min)
+
+print(1e308)
+print(1e309)  # próbáld ki!
