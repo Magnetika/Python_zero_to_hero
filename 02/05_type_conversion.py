5e4b72e9584b8f7d6323e683e2864f1bb35b1ada
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 17:40:49 +0100

    add str int string

--- 02/05_type_conversion.py
+++ 02/05_type_conversion.py
@@ -1,3 +1,17 @@
 first_name = 'Gergely'
 last_name = 'Gáll'
 
+print(first_name, last_name)
+
+full_name= first_name + ' ' + last_name
+print(full_name)
+print(full_name*2)
+
+str_value = '10'
+print(int(str_value) + 10)
+print(str_value + str(10))
+
+int_value = 10
+print(float(int_value))
+print(type(float(int_value)))
+
