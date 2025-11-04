66278c3a0cb96d1b1fd3820aab07b2b91d6ad005
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 16:33:16 +0100

    add snake case

--- 02/01_variables.py
+++ 02/01_variables.py
@@ -11,3 +11,56 @@ print(help('keywords '))
 
 PI = 3.14
 print(PI)
+
+name = "Anna"
+age = 25
+height = 1.68
+print(name, age, height)
+
+counter = 1
+print(counter)
+counter = counter + 1
+print(counter)
+counter += 1  
+print(counter)
+
+
+snake_case = "recommended"
+camelCase = "usedInOtherLanguages"
+PascalCase = "usedForClasses"
+UPPER_CASE = "constant"
+print(snake_case, camelCase, PascalCase, UPPER_CASE)
+
+a = 500
+b = 600
+c = a + b
+print(c)
+
+
+salary_january = 500
+salary_february = 600
+total_salary = salary_january + salary_february
+print(total_salary)
+
+data = 42
+print(data, type(data))
+data = "forty-two"
+print(data, type(data))
+data = 3.14
+print(data, type(data))
+
+
+Name: Anna Kovács
+Age: 19
+Student: True
+
+
+book_title = "Python Crash Course"
+author = "Eric Matthes"
+year_published = 2019
+print(f"My favorite book is {book_title} by {author} ({year_published}).")
+
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+print(f"Hello {name}, next year you will be {age + 1} years old!")
