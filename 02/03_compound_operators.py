39f73e213b70bbb5139dbc22ee50f6bb08bdf78f
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 16:58:31 +0100

    add compound file and vari

---
+++ 02/03_compound_operators.py
@@ -0,0 +1,20 @@
+age = 23+16
+age=age+1
+print(age)
+
+age +=1
+print(age)
+age -=1
+print(age)
+age *=2
+print(age)
+age /=2
+print(age)
+age //=2
+print(age)
+age %=2
+print(age)
+age +=10
+print(age)
+age **=2
+print(age)
