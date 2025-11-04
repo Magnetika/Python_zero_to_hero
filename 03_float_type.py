4b11d35b15d26bbf8c6a5f7b7382a1535aa70fa7
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 15:13:00 +0100

    declare variables

---
+++ 03_float_type.py
@@ -0,0 +1,19 @@
+print(10.1)
+print(-10.1)
+
+# min value
+print(-1.79e308)
+# -inf
+print(-1.8e308)
+
+# max value
+print(1.79e308)
+# +inf
+print(1.8e308)
+
+# closest non zero value
+print(5e-324)
+# zero
+print(1e-325)
+
+print(type(10.1))
