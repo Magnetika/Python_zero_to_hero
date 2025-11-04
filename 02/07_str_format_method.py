9fe85728dcce4601675409a537f22bd7bfe69abb
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 18:03:13 +0100

    add format type

--- 02/07_str_format_method.py
+++ 02/07_str_format_method.py
@@ -1,7 +1,11 @@
 name = 'Agnes' 
 age = 32
 
-sentence = 'My name is ' + name + ' and I am ' + str(age) + ' years old.'
+# sentence = 'My name is ' + name + ' and I am ' + str(age) + ' years old.'
+# sentence = 'My name is {} and I am {} years old.'.format(name, age)
+sentence = 'My name is {0} and I am {1} years old.Call me {0}.'.format(name, age)
 
 print(sentence)
 
+PI = 3.14159
+print('The value of PI is: {:.2f}.'.format(PI))
