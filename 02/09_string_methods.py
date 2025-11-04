99c033ad0e77277dae78434da16ff61cc661395b
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 18:17:46 +0100

    add count and replace

--- 02/09_string_methods.py
+++ 02/09_string_methods.py
@@ -8,3 +8,8 @@ print(name[4])
 
 print(f'Uppercase name: {name.upper()}')
 print(f'Lowercase name: {name.lower()}')
+
+print(f'All characters are lowercase: {name.lower().islower()}')
+
+print(f'count of "A" in name: {name.count("A")}')
+print(f'replace "A" with "a": {name.replace("A", "a")}')
