032685c0be6f09a033e4ca57cc31505adb150ae3
Magnetika <agneslilla.szabo@gmail.com>
2025-11-04 17:22:38 +0100

    add basic str

---
+++ 02/04_str_type.py
@@ -0,0 +1,13 @@
+name = 'Gáll Gergely'
+print(name)
+new_name = 'Gáll Gergely'
+age = '10'
+print(type(age))
+print('Type of age', type(age))
+
+print('I\'m')
+print("I'm")
+print('"cite"')
+      
+print('first line,\nsecond line')
+
