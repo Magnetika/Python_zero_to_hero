4ba0811b028d561ed9a75f0feb5eef5dd56edb3d
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 18:45:04 +0100

    add flatten matrix

--- 05/05_list_comprehension_nested_list.py
+++ 05/05_list_comprehension_nested_list.py
@@ -1,8 +1,15 @@
-matrix = []
-for i in range(100):
-    row = []
-    for j in range(5):
-        row.append(j)
-    matrix.append(row)
+#matrix = []
+#for i in range(100):
+#    row = []
+#    for j in range(5):
+#        row.append(j)
+#    matrix.append(row)
 
+#print(matrix)
+
+
+matrix = [[j for j in range(5)] for i in range(5)]
 print(matrix)
+
+flatten_matrix = [val for sublist in matrix for val in sublist]
+print(flatten_matrix)
