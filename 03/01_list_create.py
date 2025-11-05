81dfceceb8d7b4a96555cacc7156de9461438918
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 11:21:25 +0100

    add list and prints

---
+++ 03/01_list_create.py
@@ -0,0 +1,8 @@
+yearly_salary_list = [10000, 12000, 13000, 9000, 11000, '72000', 72000.15]
+print(yearly_salary_list)
+print(len(yearly_salary_list))
+print(yearly_salary_list[0])
+print(yearly_salary_list[3:6])
+
+yearly_salary_list[5] = 72000
+print(yearly_salary_list)
