e831db99213d39cede48662c6dbe75e7a836d4ba
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 13:51:11 +0100

    add for i range to list

--- 03/09_list_with_loop.py
+++ 03/09_list_with_loop.py
@@ -1,8 +1,9 @@
 yearly_salary_list = [50000, 60000, 55000, 70000, 65000]
 
-print(yearly_salary_list[0])
-print(yearly_salary_list[1])
-print(yearly_salary_list[2])    
-print(yearly_salary_list[3])
-print(yearly_salary_list[4])
 
+
+for i in yearly_salary_list:
+    print(i)
+
+for i in range(len(yearly_salary_list)):
+    print(yearly_salary_list[i])
