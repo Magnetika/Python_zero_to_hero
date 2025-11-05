45d14ea9df33b83e8d535eca62a8a3b06ad514bc
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 13:47:35 +0100

    add list with loop file

---
+++ 03/09_list_with_loop.py
@@ -0,0 +1,8 @@
+yearly_salary_list = [50000, 60000, 55000, 70000, 65000]
+
+print(yearly_salary_list[0])
+print(yearly_salary_list[1])
+print(yearly_salary_list[2])    
+print(yearly_salary_list[3])
+print(yearly_salary_list[4])
+
