abb84693c365bc961d0ee44e67228732b5db7455
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 15:14:17 +0100

    add if mult file

--- 04/04_if_else_with_loop.py
+++ 04/04_if_else_with_loop.py
@@ -1,9 +1,10 @@
 yearly_salary_list = [50000, 60000, 55000, 70000, 65000]
 sum_high_salary = 0
 sum_low_salary = 0
+high_salary_limit = 60000
 
 for salary in yearly_salary_list:
-    if salary > 60000:
+    if salary > high_salary_limit:
         sum_high_salary += salary
     else:
         sum_low_salary += salary
