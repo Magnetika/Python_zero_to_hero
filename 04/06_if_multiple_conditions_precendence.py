25c2d89bcf3c1e6c336f72c66e3de7b52f92d7d8
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 15:51:14 +0100

    add is file

--- 04/06_if_multiple_conditions_precendence.py
+++ 04/06_if_multiple_conditions_precendence.py
@@ -4,3 +4,17 @@ rain = True
 
 if temprature > 30 or humidity > 70 and not rain:
     print('Dry wheather')
+
+# not rain          False
+# humidity < 70     False
+#                   False and False = False
+# temprature > 30   True
+#                   True or False = True
+
+if temprature > 30 or humidity < 70 and not rain:
+    print('Dry wheather')
+
+# temprature > 30   True
+#humidity < 70      False
+#                   True or False = True
+
