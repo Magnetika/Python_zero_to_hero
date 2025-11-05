e8368f40d9390fe11535679d1d251b71d1d9a32a
Magnetika <agneslilla.szabo@gmail.com>
2025-11-05 14:43:54 +0100

    add elif

--- 04/02_if_else.py
+++ 04/02_if_else.py
@@ -1,4 +1,4 @@
-yearly_salary_in_usd = 120000
+yearly_salary_in_usd = 12000
 
 if yearly_salary_in_usd > 100000:
     print('High salary')
---
+++ 04/03_if_elif_else.py
@@ -0,0 +1,8 @@
+grade = int(input("Enter your grade:"))
+
+if grade == 1:
+    print("You are in first grade")
+elif grade == 2:
+    print("You are in second grade")
+else:
+    print("You are in another grade")
