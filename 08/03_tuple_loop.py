35c9122ba89ea30493ab060ef665a5f63df81ebe
Magnetika <agneslilla.szabo@gmail.com>
2025-11-07 09:14:43 +0100

    add tuple loop

---
+++ 08/03_tuple_loop.py
@@ -0,0 +1,7 @@
+yearly_salary  = (50000, 60000, 70000, 80000, 90000)
+
+for i in yearly_salary:
+    print(i)
+
+for i in range(len(yearly_salary)):
+    print(f"index {i}, value:{yearly_salary[i]}")
