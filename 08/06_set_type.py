ea415006a78eb6cc6a7770303e3d6ba44f101b62
Magnetika <agneslilla.szabo@gmail.com>
2025-11-07 10:02:03 +0100

    git add types

---
+++ 08/06_set_type.py
@@ -0,0 +1,15 @@
+numbers = {1, 2, 3, 4, 5, 1, 2, 3}
+print(type(numbers), numbers)  
+
+numbers.add(4)
+print(numbers)
+
+numbers.update([5, 6, 7])
+print(numbers)
+
+numbers.remove(1)
+print(numbers)
+
+numbers.discard(5)
+print(numbers)
+
