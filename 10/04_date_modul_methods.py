014e77552d810f100db9ab9ee15493d366ba585b
Magnetika <agneslilla.szabo@gmail.com>
2025-11-14 14:36:06 +0100

    add date modul methods

---
+++ 10/04_date_modul_methods.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+print(datetime(2020,10,10).strftime('%Y-%m-%d'))  # 2020-10-10
+print(datetime(2020,10,10).strftime('%d/%m/%Y'))  # 10/10/2020
+print(datetime(2020,10,10).strftime('%B %d, %Y'))
+
+
