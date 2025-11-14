1630a1058f921d304d8bb442a95199c52d8451a3
Magnetika <agneslilla.szabo@gmail.com>
2025-11-14 14:11:16 +0100

    add math

--- 10/01_import.py
+++ 10/01_import.py
@@ -1,3 +1,12 @@
-import math
+#import math
 
-print(math.pi)
+#print(math.pi)
+#print(math.fabs(-120))
+#print(math.sin(90))
+#print(dir(math))
+
+from math import *
+print(pi)
+print(fabs(-120))
+print(sin(90))
+print(dir())
